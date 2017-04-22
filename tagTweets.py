87c3be2f47476b037241098ad22d6a04ad396989
Derek Roth <derek.roth17@gmail.com>
2017-04-22 15:33:09 -0400

    Tagged tweets

--- tagTweets.py
+++ tagTweets.py
@@ -3,17 +3,24 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import brown 
 
-def tag(tweet):
-  for word in tweet:
+#def tag(tweet):
+#  for word in tweet:
 
-def tagAll(filename):
+def tag(filename):
   # Get list of tweets. Each tweet is list of words. 
   tokenized_tweets = []
+  tagged_tweets = []
   with open(filename) as f:
     content = [x.strip('\n') for x in f.readlines()]
     content = list(set(content)-set(['']))
     tokenized_tweets = [word_tokenize(i) for i in content]
-  
+
+  for tweet in tokenized_tweets:
+    tagged_tweets.append(nltk.pos_tag(tweet))
+
+  return tagged_tweets
+
+'''  
   # tag each word with most likely tag
   text = [] 
   for tweet in tokenized_tweets:
@@ -24,5 +31,6 @@ def tagAll(filename):
     text.append(taggedTweet)
 
   return text
+'''
   
-tag('tweets')
+print tag('tweets')
